e5d3bd4254d3bc4e0023d97869a59240869f9539
David L. Whitehurst <dlwhitehurst@me.com>
2011-12-02 08:03:15 -0500

    application main, just stuff to do user testing

--- src/com/cht/model/acct/Main.scala
+++ src/com/cht/model/acct/Main.scala
@@ -1,34 +1,17 @@
 package com.cht.model.acct
 
 import com.cht.model.financial._
+import com.cht.collection.acct._
 
 object Main extends App {
 
-  val list = List(
-      Asset("Cash","Unknown","100.00"),
-      Expense("Office Supplies","Unknown","25.00"),
-      Expense("Books and Periodicals","Unknown","75.00"),
-      Equity("Capital","Unknown","1000.00"),
-      Revenue("Income","Unknown","750.00"))
-      
-  val iter = list.toIterator
+  var chart:ChartOfAccounts = ChartOfAccounts.create(true)
   
-  for (acct <- iter) 
-    printAccounts(acct)
-      
-  def printAccounts(account:Account) {
-    account match {
-      case Asset (a,c,d) =>
-        print ("Asset: " + a + "," + d + "\n")
-      case Liability (a,c,d) =>
-        print ("Liability: " + a + "," + d + "\n")
-      case Revenue (a,c,d) =>
-        print ("Revenue: " + a + "," + d + "\n")
-    case Expense (a,c,d) =>
-        print ("Expense: " + a + "," + d + "\n")
-      case Equity(a,c,d) =>
-        print ("Equity: " + a + "," + d + "\n")
-    }
-  }
-
+  println("A Chart of Accounts")
+  println("************************************")
+  println
+  chart.printChart
+  println("************************************")
+  println
+  println("The default chart holds " + chart.accounts.length + " accounts")
 }
